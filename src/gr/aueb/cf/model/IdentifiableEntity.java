ecbf74cd2b4031675309a8a7ceea285ecd239b07
D.Tsetsenis <dimitse@hotmail.com>
2023-06-19 20:07:46 +0300

    v0.1

---
+++ src/gr/aueb/cf/model/IdentifiableEntity.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.model;
+
+public class IdentifiableEntity {
+    private long id;
+
+    public IdentifiableEntity() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
